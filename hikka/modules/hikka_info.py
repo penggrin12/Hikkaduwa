b67e66490ac3344aa01d12f48db3a6bddb0f848f
penggrin12 <miner.sidor@gmail.com>
2024-08-17 16:14:03 +0300

    this is muuuchhh better (`.info`)

--- hikka/modules/hikka_info.py
+++ hikka/modules/hikka_info.py
@@ -4,12 +4,10 @@
 # You can redistribute it and/or modify it under the terms of the GNU AGPLv3
 # 🔑 https://www.gnu.org/licenses/agpl-3.0.html
 
-# import git
 from telethon.tl.types import Message
 from telethon.utils import get_display_name
 
 from .. import loader, utils, version
-from ..inline.types import InlineQuery
 
 
 @loader.tds
@@ -30,7 +28,7 @@ class HikkaInfoMod(loader.Module):
                 ["🥂 Fork's Github page", "https://github.com/penggrin12/Hikkaduwa"],
                 lambda: self.strings("_cfg_cst_btn"),
                 validator=loader.validators.Union(
-                    loader.validators.Series(fixed_len=2),
+                    loader.validators.Series(loader.validators.String(), fixed_len=2),
                     loader.validators.NoneType(),
                 ),
             ),
@@ -43,16 +41,6 @@ class HikkaInfoMod(loader.Module):
         )
 
     def _render_info(self, inline: bool) -> str:
-        # try:
-        #     repo = git.Repo(search_parent_directories=True)
-        #     diff = repo.git.log([f"HEAD..origin/{version.branch}", "--oneline"])
-        #     upd = (
-        #         self.strings("update_required") if diff else self.strings("up-to-date")
-        #     )
-        # except Exception:
-        #     upd = ""
-        upd = ""
-
         me = '<b><a href="tg://user?id={}">{}</a></b>'.format(
             self._client.hikka_me.id,
             utils.escape_html(get_display_name(self._client.hikka_me)),
@@ -73,7 +61,7 @@ class HikkaInfoMod(loader.Module):
                 build=commit,
                 prefix=prefix,
                 platform=platform,
-                upd=upd,
+                upd="",
                 uptime=utils.formatted_uptime(),
                 cpu_usage=utils.get_cpu_usage(),
                 ram_usage=f"{utils.get_ram_usage()} MB",
@@ -81,35 +69,15 @@ class HikkaInfoMod(loader.Module):
             )
             if self.config["custom_message"]
             else (
-                f'<b>{{}}</b>\n\n<b>{{}} {self.strings("owner")}:</b> {me}\n\n<b>{{}}'
-                f' {self.strings("commit")}:</b> {commit} on <code>{version.branch}</code>\n<b>{{}}'
-                f' {self.strings("version")}:</b> {_version}\n\n<b>{{}}'
-                f' {self.strings("prefix")}:</b> {prefix}\n<b>{{}}'
-                f' {self.strings("uptime")}:'
-                f"</b> {utils.formatted_uptime()}\n\n<b>{{}}"
-                f' {self.strings("cpu_usage")}:'
-                f"</b> <i>~{utils.get_cpu_usage()} %</i>\n<b>{{}}"
-                f' {self.strings("ram_usage")}:'
-                f"</b> <i>~{utils.get_ram_usage()} MB</i>\n<b>{{}}</b>"
-            ).format(
-                *map(
-                    lambda x: utils.remove_html(x) if inline else x,
-                    (
-                        (
-                            utils.get_platform_emoji()
-                            if self._client.hikka_me.premium and not inline
-                            else "🌘 Hikkaduwa"
-                        ),
-                        "😎",
-                        "☀️",
-                        "🌙",
-                        "⌨️",
-                        "⌛️",
-                        "⚡️",
-                        "💼",
-                        platform,
-                    ),
-                )
+                f'🌘 <b>Hikkaduwa</b>\n\n'
+                f'😎 <b>{self.strings("owner")}:</b> {me}\n\n'
+                f'☀️ <b>{self.strings("commit")}:</b> {commit} on <code>{version.branch}</code>\n'
+                f'🌙 <b>{self.strings("version")}:</b> {_version}\n\n'
+                f'⌨️ <b>{self.strings("prefix")}:</b> {prefix}\n'
+                f'⌛️ <b>{self.strings("uptime")}:</b> {utils.formatted_uptime()}\n\n'
+                f'⚡️ <b>{self.strings("cpu_usage")}:</b> <i>~{utils.get_cpu_usage()} %</i>\n'
+                f'💼 <b>{self.strings("ram_usage")}:</b> <i>~{utils.get_ram_usage()} MB</i>\n'
+                f'{platform}'
             )
         )
 
@@ -123,46 +91,18 @@ class HikkaInfoMod(loader.Module):
             else None
         )
 
-    @loader.inline_handler(
-        thumb_url="https://img.icons8.com/external-others-inmotus-design/344/external-Moon-round-icons-others-inmotus-design-2.png"
-    )
-    @loader.inline_everyone
-    async def info(self, _: InlineQuery) -> dict:
-        """Send userbot info"""
-
-        return {
-            "title": self.strings("send_info"),
-            "description": self.strings("description"),
-            **(
-                {"photo": self.config["banner_url"], "caption": self._render_info(True)}
-                if self.config["banner_url"]
-                else {"message": self._render_info(True)}
-            ),
-            "thumb": (
-                "https://github.com/penggrin12/Hikkaduwa/raw/master/assets/hikka_pfp.png"
-            ),
-            "reply_markup": self._get_mark(),
-        }
-
     @loader.command()
     async def infocmd(self, message: Message):
-        if self.config["custom_button"]:
-            await self.inline.form(
-                message=message,
-                text=self._render_info(True),
-                reply_markup=self._get_mark(),
-                **(
-                    {"photo": self.config["banner_url"]}
-                    if self.config["banner_url"]
-                    else {}
-                ),
-            )
-        else:
-            await utils.answer_file(
-                message,
-                self.config["banner_url"],
-                self._render_info(False),
-            )
+        await self.inline.form(
+            message=message,
+            text=self._render_info(True),
+            reply_markup=self._get_mark(),
+            **(
+                {"photo": self.config["banner_url"]}
+                if self.config["banner_url"]
+                else {}
+            ),
+        )
 
     @loader.command()
     async def hikkainfo(self, message: Message):
